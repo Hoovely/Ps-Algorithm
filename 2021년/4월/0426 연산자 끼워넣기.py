97202c72c13216a8551eb8592e38d3460b06b2da
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-04-26 14:45:32 +0900

    Update 0426 연산자 끼워넣기.py

--- 2021년/4월/0426 연산자 끼워넣기.py
+++ 2021년/4월/0426 연산자 끼워넣기.py
@@ -1,5 +1,9 @@
 # 백준14888_연산자 끼워넣기_백트래킹_실버 1
+# 모르겠다.. 걍
 
 n = int(input())
 nums = list(map(int,input().split()))
-a,b,c,d = map(int, input().split())
+operators = list(map(int, input().split()))
+result = nums[0]
+for i in range(n-1):
+    result = result 연산자 nums[i+1]
