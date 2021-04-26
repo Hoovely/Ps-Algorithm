3535dad0596495942bece016eb004f53493319f5
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-04-26 11:06:35 +0900

    Update 0424 나이순 정렬.py

--- 2021년/4월/0424 나이순 정렬.py
+++ 2021년/4월/0424 나이순 정렬.py
@@ -1,11 +1,15 @@
 # 백준10814_나이순 정렬_정렬_실버 5
+# str(i)로 입력되어서 오름차순 정렬하면 9보다 10, 11이 먼저 정렬되게됨
+# 람다식으로 풀이 진행
 
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 pipple = []
 for i in range(n):
-    a,b = input().split()
-    pipple.append([int(a),str(i)+b])
-pipple.sort()
-for i in pipple:
-    print(i[0], i[1][1:])
+    a,b = sys.stdin.readline().split()
+    pipple.append((int(a), b))
+pipple_sorted = sorted(pipple, key=lambda x : x[0])
+for i in pipple_sorted:
+    print(i[0], i[1])
     
