cd070bf2a49637b08c007e01ae34c963d67bcc20
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-05-16 02:39:10 +0900

    Update 0515 숨바꼭질 6.py

--- 2021년/5월/0515 숨바꼭질 6.py
+++ 2021년/5월/0515 숨바꼭질 6.py
@@ -6,29 +6,20 @@ import sys
 
 
 def GCD(a, b):
-    while 1:
-        temp = a % b
-        if temp == 0:
-            return b
+    while b:
+        mod = a % b
         a = b
-        b = temp
+        b = mod
+    return a
 
 
 input = sys.stdin.readline
 
 n, s = map(int, input().split())
 n_lst = list(map(int, input().split()))
-# result = []
 for i in range(len(n_lst)):
     n_lst[i] = abs(s-n_lst[i])
-if n == 1:
-    # result.append(n_lst[0])
-    result = n_lst[0]
-else:
-    result = min(n_lst)
-    # for i in range(n):
-    #     for j in range(i+1, n):
-    #         a = max(n_lst[i], n_lst[j])
-    #         b = min(n_lst[i], n_lst[j])
-    #         result.append(GCD(a, b))
-print(result)
+d = min(n_lst)
+for i in range(len(n_lst)):
+    d = GCD(n_lst[i], d)
+print(d)
