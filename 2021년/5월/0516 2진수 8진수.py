9de34d04f24ddad97cb70d5ca5f425d2b67228bd
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-05-16 13:37:56 +0900

    Update 0516 2진수 8진수.py

--- 2021년/5월/0516 2진수 8진수.py
+++ 2021년/5월/0516 2진수 8진수.py
@@ -2,8 +2,4 @@
 # 2진수 -> 10진수 -> 8진수 == 시간초과
 # 파이썬 내장함수 사용
 
-import sys
-
-input = sys.stdin.readline
-num_2 = '0b' + str(input().strip())
-print(format(int(num_2, 2), 'o'))
+print(format(int(input(),2),'o'))
