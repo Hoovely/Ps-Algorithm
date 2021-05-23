42e6c6dd0a90bc040846d473b844c03c311d4367
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-05-23 21:51:57 +0900

    Update 0523 카드 구매하기.py

--- 2021년/5월/0523 카드 구매하기.py
+++ 2021년/5월/0523 카드 구매하기.py
@@ -7,11 +7,8 @@ n = int(input())
 dp = [0] + list(map(int, input().split()))
 
 for i in range(1, n+1):
-    for j in range(i-1, 0, -1):
-        if i > 2*j:
-            break
+    for j in range(1, (i//2)+1):
         if dp[j] + dp[i-j] > dp[i]:
             dp[i] = dp[j] + dp[i-j]
-
 
 print(dp[n])
