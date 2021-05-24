9c1bce299ade96718dd6d77351d20f67ef83046d
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-05-24 13:30:24 +0900

    Add files via upload

---
+++ 2021년/5월/0523 카드 구매하기 2.py
@@ -0,0 +1,14 @@
+# 백준16194_카드 구매하기 2_DP_실버 1
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] + list(map(int,input().split()))
+
+for i in range(1, n+1):
+    for j in range(1, i//2 + 1):
+        if dp[i] > dp[i-j] + dp[j]:
+            dp[i] = dp[i-j] + dp[j]
+
+print(dp[n])
