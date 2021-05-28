be64b91e087756428a41ee77c050bd85abe676a0
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-05-28 23:08:07 +0900

    Add files via upload

---
+++ 2021년/5월/0528 가장 긴 증가하는 부분 수열_오답2.py
@@ -0,0 +1,16 @@
+# 백준11053_가장 긴 증가하는 부분 수열_DP_실버 2
+# 오답 2
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+dp = [1]*n
+
+for i in range(1, n):
+    for j in range(i):
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
