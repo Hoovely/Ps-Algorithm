5e2e5090370316c5647c2023dda48f01b0a5cc55
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-05-31 21:21:29 +0900

    Add files via upload

---
+++ 2021년/5월/0531 제곱수의 합_오답1.py
@@ -0,0 +1,15 @@
+# 백준1699_제곱수의 합_DP_실버 3
+
+import math
+
+n = int(input())
+dp = [0] * (n+1)
+for i in range(1, n+1):
+    dp[i] = i
+    for j in range(1, i+1):
+        if j*j > i:
+            break
+        if dp[i] > dp[i-j*j] + 1:
+            dp[i] = dp[i-j*j] + 1
+
+print(dp[n])
