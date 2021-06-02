8d552b1405980cb1990307223d4dc56221249956
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-06-02 23:12:15 +0900

    Add files via upload

---
+++ 2021년/6월/0602 N번째 큰 수.c
@@ -0,0 +1,34 @@
+// 버블정렬 사용 
+
+#include <stdio.h>
+
+void sort(int *nums)
+{
+	int i,j,temp;
+	for (i=0;i<10;i++)
+	{
+		for(j=0;j<9-i;j++)
+		{
+			if (nums[j] > nums[j+1])
+			{
+				temp = nums[j];
+				nums[j] = nums[j+1];
+				nums[j+1] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int t,i,j,nums[10];
+	
+	scanf("%d", &t);
+	for(i=0;i<t;i++)
+	{
+		for(j=0;j<10;j++) scanf("%d", &nums[j]);
+		sort(nums);
+		printf("%d\n", nums[7]);
+	}
+	
+}
