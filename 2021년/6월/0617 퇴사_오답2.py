7c2abfc8d4463292b0f44def169608f49d74b2b5
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-06-17 23:02:00 +0900

    Add files via upload

---
+++ 2021년/6월/0617 퇴사_오답2.py
@@ -0,0 +1,17 @@
+# 백준14501_퇴사_브루트 포스_실버 4
+# 오답 2
+
+n = int(input())
+T = [0]*n
+P = [0]*n
+dp = [0]*(n+1)
+for i in range(n):
+    T[i], P[i] = map(int, input().split())
+
+for i in range(n-1, -1, -1):
+    if i+T[i] <= n:
+        dp[i] = max(P[i] + dp[i+T[i]], dp[i+1])
+    else:
+        dp[i] = dp[i+1]
+
+print(dp[0])
