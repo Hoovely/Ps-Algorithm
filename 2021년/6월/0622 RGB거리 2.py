d6a3a0c83140c28b028342c56c90f13ceabb6282
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-06-24 03:41:20 +0900

    Update 0622 RGB거리 2.py

--- 2021년/6월/0622 RGB거리 2.py
+++ 2021년/6월/0622 RGB거리 2.py
@@ -5,19 +5,27 @@ input = sys.stdin.readline
 
 n = int(input())
 rgb = []
+result = sys.maxsize
 for _ in range(n):
     rgb.append(list(map(int, input().split())))
 
-flag_1 = 0
-flag_2 = 0
-flag_3 = 0
+for index in range(3):
+    dp = [[0]*3 for _ in range(n)]
+    for i in range(3):
+        if i == index:
+            dp[0][i] = rgb[0][i]
+        else:
+            dp[0][i] = sys.maxsize
 
-for i in range(1, n):
-    if i == n-1:
-        break
-    else:
-        rgb[i][0] += min(rgb[i-1][1], rgb[i-1][2])
-        rgb[i][1] += min(rgb[i-1][0], rgb[i-1][2])
-        rgb[i][2] += min(rgb[i-1][0], rgb[i-1][1])
+    for i in range(1, n):
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb[i][0]
+        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgb[i][1]
+        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgb[i][2]
 
-print(rgb)
+    for i in range(3):
+        if i == index:
+            continue
+        else:
+            result = min(result, dp[n-1][i])
+
+print(result)
