446886574c569b64234c2ed72b91603cfa0aaec1
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-06-26 13:44:56 +0900

    Add files via upload

---
+++ 2021년/6월/0626 RGB 거리2_오답2.py
@@ -0,0 +1,33 @@
+# 백준17404_RGB거리 2_DP_골드 4
+# 오답 1
+
+from sys import stdin, maxsize
+input = stdin.readline
+
+
+n = int(input())
+rgb = []
+result = maxsize
+for _ in range(n):
+    rgb.append(list(map(int, input().split())))
+
+for index in range(3):
+    dp = [[0]*3 for _ in range(n)]
+    for i in range(3):
+        if i == index:
+            dp[0][i] = rgb[0][i]
+        else:
+            dp[0][i] = maxsize
+
+    for i in range(1, n):
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb[i][0]
+        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgb[i][1]
+        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgb[i][2]
+
+    for i in range(3):
+        if i == index:
+            continue
+        else:
+            result = min(result, dp[n-1][i])
+
+print(result)
