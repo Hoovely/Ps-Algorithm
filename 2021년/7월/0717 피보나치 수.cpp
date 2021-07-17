06e2304ecffa93e453d77315a202c40fd449d100
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-07-17 22:39:48 +0900

    Add files via upload

---
+++ 2021년/7월/0717 피보나치 수.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n) {
+    int dp[100000];
+    
+	dp[0] = 0;
+	dp[1] = 1;
+	
+	for(int i=2;i<n+1;i++)
+	{
+		dp[i] = dp[i-2]%1234567 + dp[i-1]%1234567;
+	}
+    
+    return dp[n]%1234567;
+}
+
+int main()
+{
+	cout << solution(10);
+}
