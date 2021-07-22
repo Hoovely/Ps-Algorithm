163305097cd0853a194311193a3fb4295e21089c
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-07-22 16:05:18 +0900

    Add files via upload

---
+++ 2021년/7월/0722 쉬운 계단 수_오답 1.cpp
@@ -0,0 +1,27 @@
+// 백준_10844_쉬운 계단 수_DP_실버 1
+// 오답 1
+
+#include <iostream>
+using namespace std;
+
+int n, dp[101][10];
+long long mod = 1000000000;
+int main()
+{
+	cin >> n;
+	for(int i=1;i<10;i++) dp[1][i] = 1;
+	for(int i=2;i<n+1;i++)
+	{
+		for(int j=0;j<10;j++)
+		{
+			if (j==0) dp[i][0] = dp[i-1][1];
+			else if (j==9) dp[i][9] = dp[i-1][8];
+			else dp[i][j] = (dp[i-1][j+1] + dp[i-1][j-1]) % mod;
+		}
+	}
+	
+	long long sum = 0;
+	for (int i=0;i<10;i++) sum += dp[n][i];
+	
+	cout << sum % mod;
+}
