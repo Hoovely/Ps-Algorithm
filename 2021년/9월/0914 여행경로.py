93ffda68ba2a6df282486160b322d7f0faec8a2a
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2021-09-15 11:01:09 +0900

    Add files via upload

--- 2021년/9월/0914 여행경로.py
+++ 2021년/9월/0914 여행경로.py
@@ -1,24 +1,51 @@
 # 프로그래머스_여행경로_DFS_레벨 3
 
-def dfs(now, answer, tickets):
-    answer.append(now)
-    lst = []
+answer = ["ICN"]
+
+
+def dfs(city, tickets, visited, cnt):
+    if len(tickets) == cnt:
+        return True
+
     for i in range(len(tickets)):
-        if tickets[i][1] == 'visit':
+        if visited[i] == 1:
             continue
-        if tickets[i][0] == now:
-            lst.append([tickets[i][1], i])
-    if lst:
-        lst.sort()
-        next = lst[0][0]
-        tickets[lst[0][1]][1] = 'visit'
-        dfs(next, answer, tickets)
+
+        if tickets[i][0] == city:
+            visited[i] = 1
+            answer.append(tickets[i][1])
+            flag = dfs(tickets[i][1], tickets, visited, cnt+1)
+            if flag:
+                return True
+            visited[i] = 0
+    answer.pop()
+    return False
 
 
 def solution(tickets):
-    answer = []
-    dfs("ICN", answer, tickets)
+    tickets.sort()
+    visited = [0 for _ in range(len(tickets)+1)]
+    dfs("ICN", tickets, visited, 0)
     return answer
 
 
-print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
+tickets = [["ICN", "SFO"], ["ICN", "ATL"], [
+    "SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]
+print(solution(tickets))
+
+# T 1 : [["ICN", "B"], ["B", "ICN"], ["ICN", "A"], ["A", "D"], ["D", "A"]]
+# ans : ["ICN", "B", "ICN", "A", "D", "A"]
+
+# T 2: [["ICN", "AAA"], ["ICN", "AAA"], ["ICN", "AAA"], ["AAA", "ICN"], ["AAA", "ICN"]]
+# ans : ["ICN", "AAA", "ICN", "AAA", "ICN", "AAA"]
+
+# T 3 : [["ICN", "COO"], ["ICN", "BOO"], ["COO", "ICN"], ["BOO", "DOO"]]
+# ans : ["ICN", "COO", "ICN", "BOO", "DOO"]
+
+# 4번 반례 해결하니 2번 테케 통과했습니다.
+# T 4: [["ICN", "SFO"], ["SFO", "ICN"], ["ICN", "SFO"], ["SFO", "QRE"]]
+# ans : ["ICN", "SFO", "ICN", "SFO", "QRE"]
+
+# 가장 골치아픈 1번테케는 5번반례 해결후 통과했습니다.
+# T 5 : [["ICN", "BOO"], ["ICN", "COO"], ["COO", "DOO"], ["DOO", "COO"], ["BOO", "DOO"], ["DOO", "BOO"], ["BOO", "ICN"], ["COO", "BOO"]]
+# ans : ["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"]
