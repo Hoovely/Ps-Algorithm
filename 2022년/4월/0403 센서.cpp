f5a1dd666f5c76016598b9fad798b9d550a52c6e
JoongHoo <76749472+Hoovely@users.noreply.github.com>
2022-04-03 00:46:46 +0900

    Add files via upload

---
+++ 2022년/4월/0403 센서.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n, k, result = 0;
+vector<int> Nums;
+vector<int> Diff;
+
+void Input()
+{
+	cin >> n;
+	cin >> k;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		Nums.push_back(x);
+	}
+}
+
+void Solve()
+{
+	sort(Nums.begin(), Nums.end());
+	for (int i = 0; i < n - 1; i++) Diff.push_back(Nums[i + 1] - Nums[i]);
+	sort(Diff.begin(), Diff.end());
+	for (int i = 0; i < n - k; i++) result += Diff[i];
+}
+
+void Print()
+{
+	cout << result;
+}
+
+int main()
+{
+	Input();
+	Solve();
+	Print();
+}
